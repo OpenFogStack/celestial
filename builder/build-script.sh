5c93ee5f4c2ef27b4c987c556c807b27860b0802
Tobias Pfandzelter <pfandzelter@campus.tu-berlin.de>
2021-11-12 17:37:43 +0100

    fix build script

--- builder/build-script.sh
+++ builder/build-script.sh
@@ -23,36 +23,31 @@ mkdir -p ./tmp
 
 cp -r  minirootfs/* ./tmp/
 
+# if you don't do this, apk can't access its repositories
+cp /etc/resolv.conf ./tmp/etc/resolv.conf
+cp interfaces ./tmp/etc/network/interfaces
+cp inittab ./tmp/etc/inittab
+cp start-script ./tmp/start.sh
+cp /app.sh ./tmp/app.sh
+
 if [ -d "/files" ]; then
     cp -rv /files/* ./tmp/
 fi
 
-# if you don't do this, apk can't access its repositories
-ls ./tmp/etc/
-cat ./tmp/etc/resolv.conf
-echo nameserver 2001:4860:4860::8888 > ./tmp/etc/resolv.conf
-ping -c 4 dl-cdn.alpinelinux.org
-
 cat > ./tmp/prepare.sh <<EOF
-cat /etc/resolv.conf
-ip a s
-ping -c 4 151.101.114.133
-ping -c 4 8.8.8.8
-ping -c 4 dl-cdn.alpinelinux.org
-ping -c 4 google.com
-wget https://dl-cdn.alpinelinux.org/alpine/v3.14/main/x86_64/APKINDEX.tar.gz
-apk -vv update
 passwd root -d root
-# apk add -uvv openrc ca-certificates
+apk add -u openrc ca-certificates
 exit
 EOF
 
 chroot ./tmp/ /bin/sh /prepare.sh
 
-cp interfaces ./tmp/etc/network/interfaces
-cp inittab ./tmp/etc/inittab
-cp start-script ./tmp/start.sh
-cp /app.sh ./tmp/app.sh
+if [ -f "/base.sh" ]; then
+    cp /base.sh ./tmp/base.sh
+    chroot ./tmp/ /bin/sh base.sh
+    rm ./tmp/base.sh
+fi
+
 # these are the mount points we need to create
 mkdir -p ./tmp/overlay/root \
     ./tmp/overlay/work \
@@ -64,6 +59,9 @@ if [ -f "/base.sh" ]; then
     chroot ./tmp/ /bin/sh base.sh
     rm ./tmp/base.sh
 fi
+
+# now switch back to a public name server
+echo nameserver 1.1.1.1 > ./tmp/etc/resolv.conf
 
 rm ./tmp/prepare.sh
 
